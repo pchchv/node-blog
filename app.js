ecfd4c7b8de0dd4aec26152eda21474645b1ec6f
pchchv <ipchchv@gmail.com>
2022-01-13 16:37:03 +0300

    refactor(app): Add some request data logging to the console

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.set('view engine', 'ejs');
 
 app.listen(3000);
 
+app.use((req, res) => {
+    console.log('new request made: ');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+});
+
 app.get('/', (req, res) => {
     const blogs = [];
     res.render('index', { title: 'Home' , blogs});
