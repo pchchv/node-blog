b7daec504254d804509156b675de3100c76c33ea
pchchv <ipchchv@gmail.com>
2022-01-13 15:39:16 +0300

    fix(app): fix rendering create.ejs

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
 app.use((req, res) => {
     res.status(404).render('404');
 });
